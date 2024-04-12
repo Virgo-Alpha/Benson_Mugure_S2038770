9b221d8e33a4f646e6b4ceb7ba4088a45633b669
Benson Mugure <b.mugure@alustudent.com>
2024-04-12 20:33:10 +0400

    run instructions

---
+++ README.md
@@ -0,0 +1,9 @@
+# To Run Android Studio
+
+```bash
+cd ~/../../usr/local/android-studio/bin
+```
+
+```bash
+sudo ./studio.sh
+```
--- app/src/main/java/org/me/gcu/benson_mugure_s2038770/MainActivity.java
+++ app/src/main/java/org/me/gcu/benson_mugure_s2038770/MainActivity.java
@@ -1,77 +1,126 @@
+//
+// Name                 Benson Mugure
+// Student ID           S2038770
+// Programme of Study   Computing
+//
+
+// // UPDATE THE PACKAGE NAME to include your Student Identifier
 package org.me.gcu.benson_mugure_s2038770;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
-import org.me.gcu.benson_mugure_s2038770.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity implements OnClickListener
+{
+    private TextView rawDataDisplay;
+    private Button startButton;
+    private String result;
+    private String url1="";
+    private String urlSource="https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Set up the raw links to the graphical components
+        rawDataDisplay = (TextView)findViewById(R.id.rawDataDisplay);
+        startButton = (Button)findViewById(R.id.startButton);
+        startButton.setOnClickListener(this);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // More Code goes here
+    }
 
-        setSupportActionBar(binding.toolbar);
+    public void onClick(View aview)
+    {
+        startProgress();
+    }
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    public void startProgress()
+    {
+        // Run network access on a separate thread;
+        new Thread(new Task(urlSource)).start();
+    } //
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+    // Need separate thread to access the internet resource over network
+    // Other neater solutions should be adopted in later iterations.
+    private class Task implements Runnable
+    {
+        private String url;
+
+        public Task(String aurl)
+        {
+            url = aurl;
+        }
+        @Override
+        public void run()
+        {
+
+            URL aurl;
+            URLConnection yc;
+            BufferedReader in = null;
+            String inputLine = "";
+
+
+            Log.e("MyTag","in run");
+
+            try
+            {
+                Log.e("MyTag","in try");
+                aurl = new URL(url);
+                yc = aurl.openConnection();
+                in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+                while ((inputLine = in.readLine()) != null)
+                {
+                    result = result + inputLine;
+                    Log.e("MyTag",inputLine);
+
+                }
+                in.close();
             }
-        });
-    }
+            catch (IOException ae)
+            {
+                Log.e("MyTag", "ioexception");
+            }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            //Get rid of the first tag <?xml version="1.0" encoding="utf-8"?>
+            int i = result.indexOf(">");
+            result = result.substring(i+1);
+            //Get rid of the 2nd tag <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+            i = result.indexOf(">");
+            result = result.substring(i+1);
+            Log.e("MyTag - cleaned",result);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+			//
+            // Now that you have the xml data you can parse it			
+			//
+			
+			
+            // Now update the TextView to display raw XML data
+            // Probably not the best way to update TextView
+            // but we are just getting started !
+
+            MainActivity.this.runOnUiThread(new Runnable()
+            {
+                public void run() {
+                    Log.d("UI thread", "I am the UI thread");
+                    rawDataDisplay.setText(result);
+                }
+            });
         }
 
-        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
